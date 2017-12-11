2a673a044d16150ee2205f27a523d0903c7a6b25
Giang, Thai Nguyen Minh <14520231@gm.uit.edu.vn>
2017-12-12 04:25:33 +0700

    Change WordCount.java to src/main/java

--- src/main/java/WordCount.java
+++ src/main/java/WordCount.java
@@ -1,3 +1,15 @@
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import scala.Tuple2;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
 public class WordCount {
     public static void main(String[] args) throws IOException {
         // Check inputs
